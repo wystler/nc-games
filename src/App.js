f9ac4b5528d73379dbcf35be15af9f89e47e976f
wystler <benjamin.rudd@live.com>
2022-11-01 10:33:03 +0000

    add Reviews component and route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './css/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Reviews from './components/Reviews.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-            
+            <Route path='/category' element={<Reviews/>} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,10 @@
+const Reviews = () => {
+
+    return (
+        <div>
+            <h1>something</h1>
+        </div>
+    )
+}
+
+export default Reviews
