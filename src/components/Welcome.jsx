379d759cb7d18ff3f5f4cb88740ec6b2853c6c3f
wystler <benjamin.rudd@live.com>
2022-11-01 19:32:35 +0000

    add placeholder front page with link to reviews

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Welcome = () => {
+
+    return (
+        <nav>
+            <Link to="/category/all">Look at the reviews</Link>
+        </nav>
+    )
+}
+
+export default Welcome;
