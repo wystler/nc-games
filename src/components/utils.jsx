bf965f98d51ac184246a302fde41ff4b62cf0e52
wystler <benjamin.rudd@live.com>
2022-11-01 19:31:59 +0000

    remove a console.log

--- src/components/utils.jsx
+++ src/components/utils.jsx
@@ -4,7 +4,6 @@ axios.defaults.baseURL = "https://nc-board-game-api.herokuapp.com/api/"
 export const fetchReviews = (category) => {
     let queryString = ""
     if (category!="all") queryString = `?category=${category}`
-    console.log(queryString)
     return axios.get(`reviews${queryString}`)
         .then((res) => {
             return res.data})
