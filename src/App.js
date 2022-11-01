a0a7d2ef9e60ec1b05d449358813043353830ec7
wystler <benjamin.rudd@live.com>
2022-11-01 10:25:22 +0000

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './css/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+            
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
