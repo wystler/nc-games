b8849988a6166108f6cabf080dee0e02c6831020
wystler <benjamin.rudd@live.com>
2022-11-01 19:05:06 +0000

    add other category type options

--- src/components/ReviewFilter.jsx
+++ src/components/ReviewFilter.jsx
@@ -9,9 +9,16 @@ const ReviewFilter = () => {
     }
 
     return (
-        <div>
+        <div id="reviewFilter">
             <select onChange={handleChange}>
                 <option value="all">All</option>
+                <option value="dexterity">Dexterity</option>
+                <option value="strategy">Strategy</option>
+                <option value="hidden-roles">Hidden Roles</option>
+                <option value="push-your-luck">Push your luck</option>
+                <option value="roll-and-write">Roll and write</option>
+                <option value="deck-building">Deck building</option>
+                <option value="engine-building">Engine building</option>
             </select>
         </div>
     )
