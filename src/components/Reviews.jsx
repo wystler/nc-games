c7b89c00760482111e4a3a500c17e512ddb3ab93
wystler <benjamin.rudd@live.com>
2022-11-01 18:56:07 +0000

    get parametric endpoint from path and invoke fetchReviews with it

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,20 +1,24 @@
 import { useEffect, useState } from 'react'
 import { fetchReviews } from './utils.jsx'
 import ReviewCard from './ReviewCard.jsx'
+import ReviewFilter from './ReviewFilter.jsx'
+import { useParams } from 'react-router-dom'
 
 const Reviews = () => {
 
+    const { category } = useParams()
     const [reviews, setReviews] = useState([])
 
     useEffect(() => {
-        fetchReviews()
+        fetchReviews(category)
             .then((reviewArray) => {
                 setReviews(reviewArray)
             })
-    }, [])
+    }, [category])
 
     return (
         <div id="reviews">
+            {<ReviewFilter />}
             {reviews.map(review => {return <ReviewCard key={review.review_id} review={review}/>})}
         </div>
     )
