b1539915cdcc295d61de2baec18aa5f6da7a2e4a
wystler <benjamin.rudd@live.com>
2022-11-02 11:06:25 +0000

    add working SelectedReview function with state for storing currently selected review

--- src/components/SelectedReview.jsx
+++ src/components/SelectedReview.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react'
+import { fetchReviewById } from '../utils.jsx'
+import ReviewCard from './ReviewCard.jsx'
+import { useParams } from 'react-router-dom'
+
 const SelectedReview = () => {
 
+    const { review_id } = useParams()
+    const [review, setReview] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetchReviewById(review_id)
+            .then((selectedReview) => {
+                setIsLoading(false)
+                setReview(selectedReview)
+            })
+    }, [])
+
     return (
-        <div>
-            a single review
+        <div id="selectedReview">
+            {isLoading ? <p className="loadingMessage">Loading</p> : 
+            <ReviewCard key={review.review_id} review={review} />}
         </div>
     )
 }
