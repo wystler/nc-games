4bf53ae6ae558ccd360e573543330e09c3ce1820
wystler <benjamin.rudd@live.com>
2022-11-02 08:30:53 +0000

    add label to selector

--- src/components/ReviewFilter.jsx
+++ src/components/ReviewFilter.jsx
@@ -14,16 +14,18 @@ const ReviewFilter = () => {
 
     return (
         <div id="reviewFilter">
-            <select onChange={handleChange}>
-                <option value="">All</option>
-                <option value="dexterity">Dexterity</option>
-                <option value="strategy">Strategy</option>
-                <option value="hidden-roles">Hidden Roles</option>
-                <option value="push-your-luck">Push your luck</option>
-                <option value="roll-and-write">Roll and write</option>
-                <option value="deck-building">Deck building</option>
-                <option value="engine-building">Engine building</option>
-            </select>
+            <label id="reviewFilterSelecter">Choose a game category
+                <select onChange={handleChange}>
+                    <option value="">All</option>
+                    <option value="dexterity">Dexterity</option>
+                    <option value="strategy">Strategy</option>
+                    <option value="hidden-roles">Hidden Roles</option>
+                    <option value="push-your-luck">Push your luck</option>
+                    <option value="roll-and-write">Roll and write</option>
+                    <option value="deck-building">Deck building</option>
+                    <option value="engine-building">Engine building</option>
+                </select>
+            </label>
         </div>
     )
 }
