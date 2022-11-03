1a76b975326d8ea80e60ae5259a4857b73a82436
wystler <benjamin.rudd@live.com>
2022-11-03 14:38:46 +0000

    make form a controlled component

--- src/components/AddCommentForm.jsx
+++ src/components/AddCommentForm.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
+
 const AddCommentForm = (props) => {
 
     const {username} = props
+    const [commentBody, setCommentBody] = useState("")
+
+    const handleSubmitNewComment = (event) => {
+        event.preventDefault()
+        console.log(commentBody)
+    }
+
     return (
-        <form id="addCommentForm">
+        <form id="addCommentForm" onSubmit={handleSubmitNewComment}>
             <p>Add comment as {username}</p>
-            <input id="newCommentTextArea" type="textarea" name="body"></input>
-            <input type="submit" value="Submit new comment"></input>
+            <input type="textarea" value={commentBody} onChange={(event) => setCommentBody(event.target.value)} id="newCommentTextArea"></input>
+            <button type="submit" id="submitNewCommentButton">Submit your comment</button>
         </form>
     )
 }
