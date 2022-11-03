f925b9bc03d32b26777de23309f3bd54cae679ec
wystler <benjamin.rudd@live.com>
2022-11-03 09:55:51 +0000

    add commentCard function

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,10 @@
+const CommentCard = (props) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CommentCard;
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,25 @@
-const Comments = () => {
+import CommentCard from "./CommentCard";
+import { useState, useEffect } from 'react'
+import { fetchComments } from '../utils.jsx'
+
+const Comments = (props) => {
+
+    const { review_id } = props
+    const [comments, setComments] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetchComments(review_id)
+            .then((commentArray) => {
+                setIsLoading(false)
+                setComments(commentArray)
+            })
+    }, [review_id])
 
     return (
-        <div>
-            some comments
+        <div id="comments">
+            {isLoading ? <p className="loadingMessage">Loading</p> : 
+            comments.map(comment => {return <CommentCard key={review_id} comment={comment} />})}
         </div>
     )
 }
