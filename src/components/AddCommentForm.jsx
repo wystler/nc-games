110bcf667452e3f49f67d49184f4105b44983378
wystler <benjamin.rudd@live.com>
2022-11-03 17:06:45 +0000

    add error handling if adding comment fails and locks out the submit button until a response is received

--- src/components/AddCommentForm.jsx
+++ src/components/AddCommentForm.jsx
@@ -1,21 +1,37 @@
 import { useState } from 'react'
+import { addComment } from '../utils.jsx'
 
 const AddCommentForm = (props) => {
 
-    const {username} = props
+    const {review_id, username} = props
     const [commentBody, setCommentBody] = useState("")
+    const [err, setErr] = useState(null)
+    const [disableSubmit, setDisableSubmit] = useState(false)
 
     const handleSubmitNewComment = (event) => {
         event.preventDefault()
-        console.log(commentBody)
+        setErr(null)
+        setDisableSubmit(true)
+        addComment(review_id, {body:commentBody, username:username})
+            .then((res) => {
+                setCommentBody("")
+                setDisableSubmit(false)
+                setErr('Adding comment was a success!')
+            })
+        .catch((err) => {
+            setDisableSubmit(false)
+            setErr('Something went wrong, please try again')
+        })
     }
 
     return (
         <form id="addCommentForm" onSubmit={handleSubmitNewComment}>
             <p>Add comment as {username}</p>
             <input type="textarea" value={commentBody} onChange={(event) => setCommentBody(event.target.value)} id="newCommentTextArea"></input>
-            <button type="submit" id="submitNewCommentButton">Submit your comment</button>
+            <button type="submit" disabled={disableSubmit} id="submitNewCommentButton">Submit your comment</button>
+            <p>{err}</p>
         </form>
+        
     )
 }
 
