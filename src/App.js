7d3cbdb5c4a2ed184dc96dc922885239e869c5d2
wystler <benjamin.rudd@live.com>
2022-11-03 13:43:06 +0000

    add username state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './css/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Reviews from './components/Reviews.jsx'
 import SelectedReview from './components/SelectedReview.jsx'
-
+import { useState } from 'react'
+ 
 function App() {
+
+  const [username, setUsername] = useState("testUser")
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
             <Route path='/' element={<Reviews />} />
             <Route path='/category/:category' element={<Reviews />} />
-            <Route path='/review/:review_id' element={<SelectedReview />} />
+            <Route path='/review/:review_id' element={<SelectedReview username={username} />} />
         </Routes>
       </div>
     </BrowserRouter>
