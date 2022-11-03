4fb515b01dbc378dc5cad379c934c1cbe6e71bc3
wystler <benjamin.rudd@live.com>
2022-11-03 19:49:24 +0000

    add sorting option inputs

--- src/components/ReviewFilter.jsx
+++ src/components/ReviewFilter.jsx
@@ -1,10 +1,11 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const ReviewFilter = () => {
 
+    const [searchParams, setSearchParams] = useSearchParams()
     const navigate = useNavigate()
 
-    const handleChange = (event) => {
+    const handleChangeCategory = (event) => {
         if (event.target.value) {
         navigate(`/category/${event.target.value}`)
         } else {
@@ -12,10 +13,18 @@ const ReviewFilter = () => {
         }
     }
 
+    const handleChangeSort = (event) => {
+
+    }
+
+    const handleChangeOrder = (event) => {
+
+    }
+
     return (
         <div id="reviewFilter">
-            <label id="reviewFilterSelecter">Choose a game category
-                <select onChange={handleChange}>
+            <label id="reviewFilterCategory">Choose a game category
+                <select onChange={handleChangeCategory}>
                     <option value="">All</option>
                     <option value="dexterity">Dexterity</option>
                     <option value="strategy">Strategy</option>
@@ -26,6 +35,22 @@ const ReviewFilter = () => {
                     <option value="engine-building">Engine building</option>
                 </select>
             </label>
+
+            <label id="reviewFilterSortBy">Sort the reviews by
+                <select onChange={handleChangeSort}>
+                    <option value="date">Date</option>
+                    <option value="comment_count">Comment count</option>
+                    <option value="votes">Vote count</option>
+                </select>
+            </label>
+
+            <label id="reviewFilterOrder">In order
+                <select onChange={handleChangeOrder}>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                </select>
+            </label>
+
         </div>
     )
 }
