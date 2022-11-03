5828dcd7abe71179f9e696bae59b4b4fcd5f59f5
wystler <benjamin.rudd@live.com>
2022-11-03 22:08:47 +0000

    add handling for sort_by and order drop downs

--- src/components/ReviewFilter.jsx
+++ src/components/ReviewFilter.jsx
@@ -1,8 +1,8 @@
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-const ReviewFilter = () => {
+const ReviewFilter = (props) => {
 
-    const [searchParams, setSearchParams] = useSearchParams()
+    const {setSortBy, setOrder} = props
     const navigate = useNavigate()
 
     const handleChangeCategory = (event) => {
@@ -14,43 +14,46 @@ const ReviewFilter = () => {
     }
 
     const handleChangeSort = (event) => {
-
+        setSortBy(event.target.value)
     }
 
     const handleChangeOrder = (event) => {
-
+        setOrder(event.target.value)
     }
 
     return (
         <div id="reviewFilter">
-            <label id="reviewFilterCategory">Choose a game category
-                <select onChange={handleChangeCategory}>
-                    <option value="">All</option>
-                    <option value="dexterity">Dexterity</option>
-                    <option value="strategy">Strategy</option>
-                    <option value="hidden-roles">Hidden Roles</option>
-                    <option value="push-your-luck">Push your luck</option>
-                    <option value="roll-and-write">Roll and write</option>
-                    <option value="deck-building">Deck building</option>
-                    <option value="engine-building">Engine building</option>
-                </select>
-            </label>
+            <div id="selectCategory">
+                <label id="reviewFilterCategory">Choose a game category
+                    <select className="filterSelecter" onChange={handleChangeCategory}>
+                        <option value="">All</option>
+                        <option value="dexterity">Dexterity</option>
+                        <option value="strategy">Strategy</option>
+                        <option value="hidden-roles">Hidden Roles</option>
+                        <option value="push-your-luck">Push your luck</option>
+                        <option value="roll-and-write">Roll and write</option>
+                        <option value="deck-building">Deck building</option>
+                        <option value="engine-building">Engine building</option>
+                    </select>
+                </label>
+            </div>
+            <div id="selectSorting">
+                <label id="reviewFilterSortBy">Sort the reviews by
+                    <select className="filterSelecter" onChange={handleChangeSort}>
+                        <option value="created_at">Date</option>
+                        <option value="comment_count">Comment count</option>
+                        <option value="votes">Vote count</option>
+                    </select>
+                </label>
 
-            <label id="reviewFilterSortBy">Sort the reviews by
-                <select onChange={handleChangeSort}>
-                    <option value="date">Date</option>
-                    <option value="comment_count">Comment count</option>
-                    <option value="votes">Vote count</option>
-                </select>
-            </label>
-
-            <label id="reviewFilterOrder">In order
-                <select onChange={handleChangeOrder}>
-                    <option value="asc">Ascending</option>
-                    <option value="desc">Descending</option>
-                </select>
-            </label>
-
+                <label id="reviewFilterOrder">in
+                    <select className="filterSelecter" onChange={handleChangeOrder}>
+                        <option value="asc">Ascending</option>
+                        <option value="desc">Descending</option>
+                    </select>
+                    order
+                </label>
+            </div>
         </div>
     )
 }
