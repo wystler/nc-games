e122fede356274d98b1b8c9a29aa136adfa09dcd
wystler <benjamin.rudd@live.com>
2022-11-04 13:43:27 +0000

    add route for non-existant paths and way back home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './css/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Reviews from './components/Reviews.jsx'
 import SelectedReview from './components/SelectedReview.jsx'
- 
+import ReturnHome from './components/ReturnHome.jsx' 
+
 function App() {
 
   const username = "cooljmessy"
@@ -15,6 +16,7 @@ function App() {
             <Route path='/' element={<Reviews />} />
             <Route path='/category/:category' element={<Reviews />} />
             <Route path='/review/:review_id' element={<SelectedReview username={username} />} />
+            <Route path='*' element={<ReturnHome username={username} />} />
         </Routes>
       </div>
     </BrowserRouter>
